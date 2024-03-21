582f232c5cab90d714b726814dcb37af03c99cf6
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2024-03-22 04:34:02 +0530

    drag and drop

--- src/app/upload-form/upload-form.component.ts
+++ src/app/upload-form/upload-form.component.ts
@@ -18,14 +18,14 @@ export class UploadFormComponent {
 
   constructor() {}
 
-  onFileSelected(event: any) {
+  onFileSelected(event: any, inputFile: File | null) {
     this.outputBoxVisible = false;
     this.progress = `0%`;
     this.uploadResult = '';
     this.fileName = '';
     this.fileSize = '';
-    this.uploadStatus =undefined;
-    const file: File = event.target.files[0];
+    this.uploadStatus = undefined;
+    const file: File = inputFile || event.target.files[0];
 
     if (file) {
       this.fileName = file.name;
@@ -39,7 +39,6 @@ export class UploadFormComponent {
       xhr.open('POST', 'http://localhost:4000/upload', true);
 
       xhr.upload.onprogress = (progressEvent) => {
-        console.log({ progressEvent });
         if (progressEvent.lengthComputable) {
           const progress = (progressEvent.loaded / progressEvent.total) * 100;
           this.progress = `${Math.round(progress)}%`;
@@ -51,7 +50,6 @@ export class UploadFormComponent {
           if (xhr.status === 200) {
             this.uploadResult = 'Uploaded';
           } else if (xhr.status === 400) {
-            console.log({xhr})
             this.uploadResult = JSON.parse(xhr.response)!.message;
           } else {
             this.uploadResult = 'File upload failed!';
@@ -69,16 +67,11 @@ export class UploadFormComponent {
     event.stopPropagation();
   }
 
-  handleDragLeave(event: DragEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-  }
-
   handleDrop(event: DragEvent) {
     event.preventDefault();
-    event.stopPropagation();
-    console.log({ event });
-    const file: File = event.dataTransfer!.files[0];
-    this.onFileSelected(file);
+    if (event.dataTransfer) {
+      const file: File = event.dataTransfer.files[0];
+      this.onFileSelected(event, event.dataTransfer.files[0]);
+    }
   }
 }
