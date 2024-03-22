9d31f5245c9387ac402702202e925edf4c873dac
Abhinav Dhiman <dhiman.ahnv@gmail.com>
2024-03-22 12:58:58 +0530

    feat: added homepage for codesandbox
    Signed-off-by: Abhinav Dhiman <dhiman.ahnv@gmail.com>

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <title>Upload Files</title>
+    <style>
+        /* Add styles here */
+        * {
+            box-sizing: border-box;
+            font-family: "Montserrat", sans-serif;
+            font-optical-sizing: auto;
+            font-style: normal;
+        }
+
+        body {
+            margin: 0;
+            padding: 1rem;
+            background-color: #0679e4;
+        }
+
+        .container {
+            display: flex;
+            max-width: 300px;
+            padding: 20px;
+            flex-flow: column;
+            margin: 100px auto 1rem auto;
+            background-color: #fff;
+            box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.15);
+            gap: 1rem;
+        }
+
+        .container h1 {
+            margin: 0;
+            padding: 1rem 0px 0.5rem;
+            font-size: 1.25rem;
+            font-weight: 700;
+            color: #434850;
+            text-align: left;
+        }
+
+        .container a {
+            text-decoration: none;
+            color: #0679e4;
+            border: 1px dashed #333333;
+            border-radius: 0.25rem;
+            padding: 0.5rem;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+
+        <h1>PHP Upload file demo</h1>
+
+        <a href="native">Native PHP Upload Demo</a>
+        <a href="imagekit">ImageKit PHP Upload Demo</a>
+
+        </form>
+</body>
+
+</html>
