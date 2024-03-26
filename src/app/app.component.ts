864301f3c1f2e73a58180487fc82cddfda0223f9
Rishav Bhowmik <80034794+rishavbhowmik@users.noreply.github.com>
2024-03-26 15:56:52 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { VideoPlayerComponent } from "./video-player/video-player.component";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  standalone: true,
-  imports: [RouterOutlet, VideoPlayerComponent],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = "video-player-demo";
+  title = 'CodeSandbox';
 }
